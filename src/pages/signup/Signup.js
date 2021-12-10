dc0ab299924bacd744fbeb784451927063539312
naMqe <naMqe07@gmail.com>
2021-12-10 02:12:17 +0100

    fixed react import error

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development'
+import { useState } from 'react'
 import { useSignup } from '../../hooks/useSignup' 
 import styles from './Signup.module.css'
 
