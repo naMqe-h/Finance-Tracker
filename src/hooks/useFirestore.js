04f1c36a497b0cd5920b69f3da5798d69eb9c616
naMqe <naMqe07@gmail.com>
2021-12-16 23:41:59 +0100

    fixed typo in useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -40,7 +40,7 @@ export const useFirestore = (collection) => {
         try {
             const createdAt = timestamp.fromDate(new Date())
             const addedDocument = await ref.add({ ...doc,  createdAt })
-            dispatchIfNotCancelled({ type: 'ADDED_DUCOMENT', payload: addedDocument })
+            dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
         } catch (err) {
             dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
         }
